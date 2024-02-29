09326952014572ace27d0adb615e1295715eea11
Santhosh <pitakasanthosh@gmail.com>
2024-02-29 10:22:03 +0530

    Add header and footer components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <>
-    <footer>
-      
-    </footer>
+      <footer>
+        <div className="footer-container">
+          <h2>Santhosh mani</h2>
+          <p>&copy; 2024 Santhosh mani</p>
+        </div>
+        <div className="socials-container">
+          <div className="social">
+            <FaLinkedin />
+          </div>
+          <div className="social">
+            <FaGithub />
+          </div>
+          <div className="social">
+            <FaTwitter />
+          </div>
+        </div>
+      </footer>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <header>
+        <h1>Santhosh mani</h1>
+        <nav>
+          <button className="mobile">
+            <span className="material-icons">menu</span>
+          </button>
+          <ul>
+            <li>
+              <a href="/#">Home</a>
+            </li>
+            <li>
+              <a href="/#skills">Skills</a>
+            </li>
+            <li>
+              <a href="/#projects">Projects</a>
+            </li>
+            <li>
+              <a href="/#reviews">Reviews</a>
+            </li>
+            <li>
+              <a href="/#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
